13f9754ca06194bc0b23b9825e130276f7401494
Joseth Stiven Aguilar Enciso <stivenaguilar10288@gmail.com>
2025-09-03 15:45:23 -0500

    agregue funcion de cambiar a estrella

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const figura = document.getElementById('figure');
+
+
+    function circulo() {
+        figura.style.borderRadius= "50%";
+        figura.style.background= "red";
+    }
+
+    function estrella() {
+        figura.style.clipPath = "polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)";
+    }
+
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('btnCircle').addEventListener('click', circulo);
+    document.getElementById('btnStar').addEventListener('click', estrella);
+
+})
