bd9197ffcb2c7d5bb312e6cc8581c9982374273f
Joseth Stiven Aguilar Enciso <stivenaguilar10288@gmail.com>
2025-09-03 15:52:23 -0500

    agregue la funcion de un gif de fondo

--- js/main.js
+++ js/main.js
@@ -10,6 +10,12 @@ const figura = document.getElementById('figure');
         figura.style.clipPath = "polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)";
     }
 
+    function gif() {
+        figura.style.backgroundImage = "url(img/XOsX.gif)";
+        figura.style.backgroundSize = "cover";
+
+    }
+
 
 
 
@@ -22,5 +28,7 @@ const figura = document.getElementById('figure');
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('btnCircle').addEventListener('click', circulo);
     document.getElementById('btnStar').addEventListener('click', estrella);
+    document.getElementById('btnGif').addEventListener('click', gif);
+
 
 })
